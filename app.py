e6b8d1ce51f9da9c9ab349d6d07820b7372054a6
arthurOcg <arthur.lima.oliveira@ccc.ufcg.edu.br>
2019-04-12 00:33:36 -0300

    tirando debug

--- app.py
+++ app.py
@@ -16,4 +16,4 @@ def comecando():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True, use_reloader=True)
+    app.run(host='0.0.0.0', port=port)
