62024313d71ab859d817e68ac89fc83a3f87094b
arthurOcg <arthur.lima.oliveira@ccc.ufcg.edu.br>
2019-04-12 23:54:48 -0300

    Arquivos atualizados

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def hello():
 
 @app.route('/oi', methods=['GET'])
 def comecando():
-    return {"Só vai assim!"}, 200
+    return jsonify({"Mensagem": "Só vai assim!"}), 200
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
