795e48ffbd4a6afd06e96279d75a27ae2c98bc18
arthurOcg <arthur.lima.oliveira@ccc.ufcg.edu.br>
2019-04-15 23:20:29 -0300

    Implementado metodo post para retornar histograma da imagem recebida usando OpenCV

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import os
+import base64
+import cv2
 
 app = Flask(__name__)
 CORS(app)
@@ -15,6 +17,50 @@ def hello():
 @app.route('/oi', methods=['GET'])
 def comecando():
     return jsonify({"Mensagem": "Só vai assim!"}), 200
+
+@app.route('/ui', methods=['POST']) 
+def geraTexto():    
+    if request.method == 'POST':
+        message = request.get_json()
+        return message['imagem'],201
+
+def geraHistograma(imagem):
+    
+    histB  =  cv2.calcHist([imagem], [0], None , [ 256 ], [ 0 , 255 ])
+    histG  =  cv2.calcHist([imagem], [1], None , [ 256 ], [ 0 , 255 ])
+    histR  =  cv2.calcHist([imagem], [2], None , [ 256 ], [ 0 , 255 ])
+
+    novaVermelha = []
+    for i in range(histR.size):
+        for j in range(1):
+            novaVermelha.append(int(histR[i][j]))
+
+    novaAzul = []
+    for i in range(histB.size):
+        for j in range(1):
+            novaAzul.append(int(histB[i][j]))
+
+    novaVerde = []
+    for i in range(histG.size):
+        for j in range(1):
+            novaVerde.append(int(histG[i][j]))
+         
+    novaResult = novaVermelha
+    novaResult.extend(novaVerde)
+    novaResult.extend(novaAzul)
+    return novaResult
+
+@app.route('/analize', methods=['POST'])
+def read_image():
+    if request.method == 'POST':
+        imagem = request.get_json()['mensagem']
+        image_data = base64.b64decode(imagem)
+        with open('temp_image.jpg', 'wb') as f:
+            f.write(image_data)
+            f.close()
+        img = cv2.imread('temp_image.jpg')
+
+        return jsonify({"histograma": geraHistograma(img)})
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
